f1ed1537666822c80caf231e20b5f3e0b1829ace
David Ford <fordd@pacden.com>
2015-09-16 11:26:57 -0500

    adding regex and slugifying

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 #! /usr/bin/env node
-
+var program = require('commander');
 var branchLinker = require('./jira-branch-server-linker');
 
-// do regex
-branchLinker.addBranchLink('API-2');
+
+program
+  .version('0.0.1')
+  .option('-a, --add-branch-link <branchname>', 'Adds a branch link to a Jira issue', addBranchLink)
+ program.parse(process.argv);
+
+function addBranchLink(branchName) {
+  console.log('add branch link with branch name ', branchName);
+  branchLinker.addBranchLink(branchName);
+}
