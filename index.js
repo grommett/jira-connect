015a4b9fb24a1caafb32742b41b2e60be3a0fbd0
David Ford <fordd@pacden.com>
2015-09-17 12:32:59 -0500

    moved jira modules to lib folder, changed addbranch signature

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 /* process global */
 var program = require('commander');
-var branchLinker = require('./jira-branch-server-linker');
+var branchLinker = require('./lib/jira-branch-server-linker');
 
 /**
  * TODO Maybe add update config command
@@ -9,10 +9,18 @@ var branchLinker = require('./jira-branch-server-linker');
 
 program
   .version('0.0.1')
-  .option('-a, --add-branch-link <branchname>', 'Adds a branch link to a Jira issue', addBranchLink)
+  .option('-a, --add-branch-link <branchUrl> <jiraIssueID>', 'Adds a branch link to a Jira issue', addBranchLink);
+  
  program.parse(process.argv);
 
-function addBranchLink(branchName) {
-  console.log('add branch link with branch name ', branchName);
-  branchLinker.addBranchLink(branchName);
+function addBranchLink(jiraIssueID, branchUrl) {
+  // console.log('add branch from Jira ID ', jiraIssueID);
+  // console.log('add branch link with branch url ', branchUrl);
+  // console.log('program.args ', program);
+  // console.log('issueID: ' + process.argv[3], ' branch link ', process.argv[4]);
+  // node-jira -a https//:branchname.pacden.com BT-999
+  branchLinker.addBranchLink(process.argv[3],  process.argv[4]);
 }
+
+// for testing ONLY!
+//addBranchLink( process.argv[2]);
